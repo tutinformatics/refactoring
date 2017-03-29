e47bf370862545b6f69fab6dd8fb63d38b3fe804
Martin Rebane <martin@linuxator.com>
2017-03-29 13:28:26 +0300

    Original ugly code

---
+++ src/rental/RentalPriceCalculator.java
@@ -0,0 +1,43 @@
+package rental;
+
+public class RentalPriceCalculator {
+	
+	// age - age of driver
+	// licence - number of full years person holds driving licence
+	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+	// acc - has s/he caused any accidents within last year
+	// acc2 - has s/he participated (but not caused) in any accidents within last year
+	// season - if it is high season or not
+	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
+		
+		if (age < 18) {
+			throw new IllegalArgumentException("Driver too young - cannot quote the price");
+		}
+		if (age <= 21 && clazz > 2) {
+			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
+		}
+		
+		double rentalprice = age;
+		
+		if (clazz >=4 && age <= 25 && season != false) {
+			rentalprice = rentalprice * 2;
+		}
+		
+		if (licence < 1) {
+			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+		}
+		
+		if (licence < 3) {
+			rentalprice = rentalprice * 1.3;
+		}
+		
+		if (acc == true && age < 30) {
+			rentalprice += 15;
+		}
+
+		if (rentalprice > 1000) {
+			return 1000.00;
+		}
+		return rentalprice;
+	}
+}
