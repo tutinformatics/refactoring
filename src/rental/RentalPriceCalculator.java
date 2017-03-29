0daf23fc8e0bd6c4d790a155e40c7802e9df7802
Margus Tanneberg <m.tanneberg@gmail.com>
2017-03-29 17:42:31 +0300

    prax 8

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -8,36 +8,56 @@ public class RentalPriceCalculator {
 	// acc - has s/he caused any accidents within last year
 	// acc2 - has s/he participated (but not caused) in any accidents within last year
 	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
+	
+	public double price(int age, int licence, int clazz, boolean acc, boolean season) {
 		
+		checkIfApplicable(age, clazz);
+		
+		double rentalprice = age;
+		
+		rentalprice = highSeasonPricing(age, clazz, season, rentalprice);
+		
+		rentalprice = checkLicense(licence, rentalprice);
+		
+		rentalprice = checkAccidents(age, acc, rentalprice);
+
+		if (rentalprice > 1000) {
+			return 1000.00;
+		}
+		return rentalprice;
+	}
+
+	public double highSeasonPricing(int age, int clazz, boolean season, double rentalprice) {
+		if (clazz >=4 && age <= 25 && season == true) {
+			rentalprice = rentalprice * 2;
+		}
+		return rentalprice;
+	}
+
+	public double checkAccidents(int age, boolean acc, double rentalprice) {
+		if (acc == true && age < 30) {
+			rentalprice += 15;
+		}
+		return rentalprice;
+	}
+
+	public double checkLicense(int licence, double rentalprice) {
+		if (licence <= 1) {
+			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+		}
+		
+		if (licence <= 3) {
+			rentalprice = rentalprice * 1.3;
+		}
+		return rentalprice;
+	}
+
+	public void checkIfApplicable(int age, int clazz) {
 		if (age < 18) {
 			throw new IllegalArgumentException("Driver too young - cannot quote the price");
 		}
 		if (age <= 21 && clazz > 2) {
 			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
 		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
-			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
-		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
-		}
-		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
-		}
-
-		if (rentalprice > 1000) {
-			return 1000.00;
-		}
-		return rentalprice;
 	}
 }
--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -1,19 +1,31 @@
 package rental;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
 	
+	private RentalPriceCalculator calculator;
+	
 	@Before
 	public void beforeEachTest() {
-		// this method is called before each test
+		calculator = new RentalPriceCalculator();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCheckIfApplicable() {
+//		assertEquals()
+	}
+	
+	@Test
+	public void testCheckLicence() {
+		assertEquals(1.3, calculator.checkLicense(2, 1.0), 0.00001);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testCheckLicence2() {
+		assertEquals(1.3, calculator.checkLicense(1, 1.0), 0.00001);
 	}
 }
