1f0d1d5013cf1a4275ad77ac43392601c9781f34
Martin Rebane <martin@linuxator.com>
2017-03-29 13:47:33 +0300

    Add test folder and empty test class

---
+++ test/rental/RentalPriceCalculatorTest.java
@@ -0,0 +1,19 @@
+package rental;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RentalPriceCalculatorTest {
+	
+	@Before
+	public void beforeEachTest() {
+		// this method is called before each test
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
