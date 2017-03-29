808643145d605808824493ec17184664a099116b
Kert Koplus <Kert Koplus>
2017-03-29 17:32:20 +0300

    refactoring and testing

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -2,42 +2,54 @@ package rental;
 
 public class RentalPriceCalculator {
 	
-	// age - age of driver
-	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
-	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
+	public double price(int driverAge, int ownedLicenceFor, int carSize, boolean hasHadAccidentLastYear, boolean hasParticipatedButNotCausedLastYear, boolean isOrNotHighSeason) {
 		
-		if (age < 18) {
+		if (howManyYearsLicenceOwned(driverAge) < 18) {
 			throw new IllegalArgumentException("Driver too young - cannot quote the price");
 		}
-		if (age <= 21 && clazz > 2) {
+		if (howManyYearsLicenceOwned(driverAge) <= 21 && howManyYearsLicenceOwned(carSize) > 2) {
 			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
 		}
 		
-		double rentalprice = age;
+		double rentalprice = howManyYearsLicenceOwned(driverAge);
 		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
+		if (howManyYearsLicenceOwned(carSize) >=4 && howManyYearsLicenceOwned(driverAge) <= 25 && isHighSeason(ifHadAccidentLastYear(isOrNotHighSeason))) {
+			rentalprice = isPriceCorrect(rentalprice) * 2;
 		}
 		
-		if (licence < 1) {
+		if (howManyYearsLicenceOwned(ownedLicenceFor) < 1) {
 			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
 		}
 		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
+		if (howManyYearsLicenceOwned(ownedLicenceFor) < 3) {
+			rentalprice = isPriceCorrect(rentalprice) * 1.3;
 		}
 		
-		if (acc == true && age < 30) {
+		if (ifHadAccidentLastYear(hasHadAccidentLastYear) == true && howManyYearsLicenceOwned(driverAge) < 30) {
 			rentalprice += 15;
 		}
 
-		if (rentalprice > 1000) {
+		if (isPriceCorrect(rentalprice) > 1000) {
 			return 1000.00;
 		}
+		return isPriceCorrect(rentalprice);
+	}
+
+	double isPriceCorrect(double rentalprice) {
 		return rentalprice;
 	}
+
+	boolean ifHadAccidentLastYear(boolean hasHadAccidentLastYear) {
+		return hasHadAccidentLastYear;
+	}
+
+	int howManyYearsLicenceOwned(int ownedLicenceFor) {
+		return ownedLicenceFor;
+	}
+
+	boolean isHighSeason(boolean season) {
+		return ifHadAccidentLastYear(season) != false;
+	}
+	
+	
 }
