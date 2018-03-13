7e0985c72c5b2a7b99ed69fd975bb63f469c49ab
Joosep Hint <joosep.hint@ekool.eu>
2018-03-13 13:33:24 +0200

    Cleaned code

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -1,43 +1,74 @@
 package rental;
 
 public class RentalPriceCalculator {
-	
-	// age - age of driver
-	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
-	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
-		
-		if (age < 18) {
-			throw new IllegalArgumentException("Driver too young - cannot quote the price");
-		}
-		if (age <= 21 && clazz > 2) {
-			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
-		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
-			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
-		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
-		}
-		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
-		}
 
-		if (rentalprice > 1000) {
-			return 1000.00;
-		}
-		return rentalprice;
-	}
+    private static final int MIN_AGE = 18;
+    private static final int MIN_AGE_FOR_CLASS_2 = 21;
+    private static final double MAX_RENTAL_PRICE = 1000;
+
+    /**
+     * Calculates rent price for user according to their data.
+     * @param driverAge - Age of the driver
+     * @param licenseHoldingYears - How many years has the driver held their license
+     * @param carClass - Class of car (1 - 5)
+     * @param hasCausedAccidents - Have they caused any accidents before.
+     * @param isHighSeason - Is high season or not.
+     * @return the calculated rental price.
+     */
+    public double calculatePrice(int driverAge, int licenseHoldingYears, int carClass,
+                                 boolean hasCausedAccidents, boolean isHighSeason) {
+        double rentalPrice = driverAge;
+
+        checkDriverAge(driverAge, carClass);
+        checkHasHeldLicenseForEnoughYears(licenseHoldingYears);
+        rentalPrice = incrementRentalPriceIfCarClassOverFour(driverAge, carClass, isHighSeason, rentalPrice);
+        rentalPrice = incrementRentalPriceIfNotHeldLicenseEnough(licenseHoldingYears, rentalPrice);
+        rentalPrice = incrementRentalPriceIfCausedAnyAccidents(driverAge, hasCausedAccidents, rentalPrice);
+
+        return limitedRentalPrice(rentalPrice);
+    }
+
+    private double incrementRentalPriceIfCarClassOverFour(int driverAge, int carClass, boolean isHighSeason, double rentalPrice) {
+        if (carClass >= 4 && driverAge <= 25 && isHighSeason) {
+            rentalPrice = rentalPrice * 2;
+        }
+        return rentalPrice;
+    }
+
+    private double incrementRentalPriceIfCausedAnyAccidents(int driverAge, boolean hasCausedAccidents, double rentalPrice) {
+        if (hasCausedAccidents && driverAge < 30) {
+            rentalPrice += 15;
+        }
+        return rentalPrice;
+    }
+
+    private double incrementRentalPriceIfNotHeldLicenseEnough(int licenseHoldingYears, double rentalPrice) {
+        if (licenseHoldingYears < 3) {
+            rentalPrice = rentalPrice * 1.3;
+        }
+        return rentalPrice;
+    }
+
+    private double limitedRentalPrice(double rentalPrice) {
+        if (rentalPrice > MAX_RENTAL_PRICE) {
+            return MAX_RENTAL_PRICE;
+        }
+
+        return rentalPrice;
+    }
+
+    private void checkHasHeldLicenseForEnoughYears(int licenseHoldingYears) {
+        if (licenseHoldingYears < 1) {
+            throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+        }
+    }
+
+    private void checkDriverAge(int driverAge, int carClass) {
+        if (driverAge < MIN_AGE) {
+            throw new IllegalArgumentException("Driver too young - cannot quote the calculatePrice");
+        }
+        if (driverAge <= MIN_AGE_FOR_CLASS_2 && carClass > 2) {
+            throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
+        }
+    }
 }
--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -1,19 +1,24 @@
 package rental;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
+
+	private RentalPriceCalculator rentalPriceCalculator;
 	
 	@Before
 	public void beforeEachTest() {
-		// this method is called before each test
+		rentalPriceCalculator = new RentalPriceCalculator();
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalArgumentException.class)
+	public void testCantRentUnderAge() {
+		rentalPriceCalculator.calculatePrice(17, 1, 1, false, false);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCanRentIs18YearsOld() {
+		rentalPriceCalculator.calculatePrice(18, 1, 1, false, false);
 	}
 }
