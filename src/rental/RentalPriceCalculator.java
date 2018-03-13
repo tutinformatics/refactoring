67d75b65e0a7acc997848370c6ea44005127a3fa
Ragnar Rebase <rrebase@gmail.com>
2018-03-13 13:06:04 +0200

    Refactor RentalPriceCalculator to conform clean code standards

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -1,43 +1,60 @@
 package rental;
 
 public class RentalPriceCalculator {
-	
-	// age - age of driver
-	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
-	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
-		
-		if (age < 18) {
-			throw new IllegalArgumentException("Driver too young - cannot quote the price");
-		}
-		if (age <= 21 && clazz > 2) {
-			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
-		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
-			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
-		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
-		}
-		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
-		}
 
-		if (rentalprice > 1000) {
-			return 1000.00;
-		}
-		return rentalprice;
-	}
+    /**
+     * Calculate the price of rental.
+     *
+     * @param age                        age of driver
+     * @param licenseAge                 number of full years person holds driving license
+     * @param carClass                   car class of the car from 1 (smallest) to 5 (largest) that person can rent
+     * @param hasCausedAccidents         true if has caused any accidents within last year
+     * @param hasParticipatedInAccidents true if has participated in accidents within last year, false otherwise
+     * @param isHighSeason               true if is high season, else false
+     * @return price of rental
+     */
+    public double calculateRentalPrice(int age,
+                                       int licenseAge,
+                                       int carClass,
+                                       boolean hasCausedAccidents,
+                                       boolean hasParticipatedInAccidents,
+                                       boolean isHighSeason) {
+
+        checkEligibility(age, licenseAge, carClass);
+
+        double rentalPrice = age;
+
+        if (carClass >= 4 && age <= 25 && isHighSeason) {
+            rentalPrice *= 2;
+        }
+        if (licenseAge < 3) {
+            rentalPrice *= 1.3;
+        }
+        if (hasCausedAccidents && age < 30) {
+            rentalPrice += 15;
+        }
+        return Math.min(rentalPrice, 1000);
+    }
+
+    /**
+     * Check eligibility of car rental.
+     * If not eligible, throw an error.
+     *
+     * @param age        age of driver
+     * @param licenseAge number of full years person holds driving license
+     * @param carClass   car class of the car from 1 (smallest) to 5 (largest) that person can rent
+     */
+    private void checkEligibility(int age, int licenseAge, int carClass) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Driver too young - cannot quote the price");
+        } else if (age <= 21 && carClass > 2) {
+            throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
+        } else if (licenseAge < 1) {
+            throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
 }
--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -6,14 +6,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
-	
-	@Before
-	public void beforeEachTest() {
-		// this method is called before each test
-	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void beforeEachTest() {
+        // this method is called before each test
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
 }
