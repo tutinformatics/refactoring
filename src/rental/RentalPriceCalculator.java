26d23d51679434587e2b77744fcc8abd8adea2ea
jagors <jagors@ttu.ee>
2018-03-14 11:43:08 +0200

    Artur, Veronika, Jaroslav

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -1,43 +1,89 @@
 package rental;
 
 public class RentalPriceCalculator {
+
+	private final int LIMITED_CAR_RENTAL_PERMISSION_AGE = 18;
+	private final int FULL_CAR_RENTAL_PERMISSION_AGE = 21;
+	private final int EXTRA_CHARGE_DISMISS_AGE = 25;
 	
-	// age - age of driver
+	// driverAge - driverAge of driver
 	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
+	// carClass - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+	// hadAccident - has s/he caused any accidents within last year
 	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
-		
-		if (age < 18) {
+	// isHighSeason - if it is high isHighSeason or not
+
+	/**
+	 *
+	 * @param driverAge Age of the driver.
+	 * @param licenseAgeInFullYears The amount of time in full years that the driver holds the license.
+	 * @param carClass The class of car (1-5).
+	 * @param hadAccident If the driver had an accident in the previous year.
+	 * @param isHighSeason If it is high season.
+	 * @return The rental price of car.
+	 */
+	public double calculateRentalPrice(int driverAge, int licenseAgeInFullYears, int carClass, boolean hadAccident, boolean isHighSeason) {
+
+		double rentalPrice = driverAge;
+
+		if (isAdult(driverAge)) {
 			throw new IllegalArgumentException("Driver too young - cannot quote the price");
 		}
-		if (age <= 21 && clazz > 2) {
+
+		if (isFullPermissionAge(driverAge, carClass)) {
 			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
 		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
+
+		if (holdsLicenseForUnderAYear(licenseAgeInFullYears)) {
 			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
 		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
+
+		if (canApplyDischarge(carClass, driverAge, isHighSeason)) {
+			rentalPrice = rentalPrice * 2;
 		}
 		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
+		if (isNotExperiencedDriver(licenseAgeInFullYears)) {
+			rentalPrice = rentalPrice * 1.3;
+		}
+		
+		if (isYoungAndFoolish(hadAccident, driverAge)) {
+			rentalPrice += 15;
 		}
 
-		if (rentalprice > 1000) {
+		if (isVeryExpensiveCar(rentalPrice)) {
 			return 1000.00;
 		}
-		return rentalprice;
+
+		return rentalPrice;
 	}
+
+	private boolean isAdult(int age) {
+		return age < LIMITED_CAR_RENTAL_PERMISSION_AGE;
+	}
+
+	private boolean isFullPermissionAge(int age, int clazz) {
+		return age <= FULL_CAR_RENTAL_PERMISSION_AGE && clazz > 2;
+	}
+
+	private boolean canApplyDischarge(int carClass, int driverAge, boolean season) {
+		return carClass >=4 && driverAge <= EXTRA_CHARGE_DISMISS_AGE && season;
+	}
+
+	private boolean holdsLicenseForUnderAYear(int licenseAgeInFullYears) {
+		return licenseAgeInFullYears < 1;
+	}
+
+	private boolean isNotExperiencedDriver(int licenseAge) {
+		return licenseAge < 3;
+	}
+
+	private boolean isYoungAndFoolish(boolean hadAccident, int driverAge) {
+		return hadAccident && driverAge < 30;
+	}
+
+	private boolean isVeryExpensiveCar(double rentalPrice) {
+		return rentalPrice > 1000;
+	}
+
+
 }
