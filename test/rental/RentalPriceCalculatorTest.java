e7580ede79b50528b4dc0444756e737b12d4b3ce
Vladimir Kulagin <vlkula@ttu.ee>
2018-03-14 23:47:43 +0200

    refactoring and tests

--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -1,19 +1,37 @@
 package rental;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
-	
+
+	RentalPriceCalculator calculator;
+
 	@Before
 	public void beforeEachTest() {
-		// this method is called before each test
+		calculator = new RentalPriceCalculator();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ifPriceGreaterThanMaxReturnMax() {
+		assertEquals(1000, calculator.price(29, 2, 5, true, false), 0.1);
+
 	}
+
+    @Test(expected = IllegalArgumentException.class)
+    public void youngerThan18Test() {
+        calculator.price(1, 1, 1, true, true);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void illegalDriverClazzTest() {
+        calculator.price(21, 2, 2, false, true);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkNumberOfFullYearsTest() {
+        calculator.price(1, 0, 1, true, false);
+    }
 }
